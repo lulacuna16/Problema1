e2ed07941140bf67adb8dd9afd4056dab2b9de3d
ZabDS <zabdiel1297@gmail.com>
2020-06-12 17:26:43 -0500

    Cliente servidor con validacion de caracteristicas por voz

--- cliente.py
+++ cliente.py
@@ -5,11 +5,48 @@ from random import shuffle
 import time
 import os
 import sys
-import json
+import pyaudio
+import wave
+from pathlib import Path
 
-HOST = "192.168.1.64"  # The server's hostname or IP address
+HOST = "localhost"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 buffer_size = 1024
+
+def GrabarPregunta():
+    chunk = 1024 
+    sample_format = pyaudio.paInt16  
+    channels = 2
+    fs = 44100  
+    seconds = 3
+    filename = "audio.wav"
+
+    p = pyaudio.PyAudio()  
+
+    print('Grabando')
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+    frames = []      
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    
+    stream.stop_stream()
+    stream.close()
+    
+    p.terminate()
+
+    print('Terminando de grabar')
+        
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
     TCPClientSocket.connect((HOST, PORT))
@@ -39,7 +76,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
     while True:
         print("Espere su turno: ")
         data = TCPClientSocket.recv(buffer_size)
-        coord=str(input("Ingrese un mensaje para enviar al servidor "))
-        TCPClientSocket.sendall(coord.encode())
+        coord=input("Pulse enter para grabar ")
+        #TCPClientSocket.sendall(coord.encode())
+        ##########################################
+        GrabarPregunta()
+        TamAud=Path("audio.wav").stat().st_size
+        print(str(TamAud))
+        TCPClientSocket.sendall(str(TamAud).encode())
+        with open("audio.wav", 'rb') as f:
+          for l in f: 
+            TCPClientSocket.sendall(l)
+        print("Terminando de enviar archivo")
         respuesta = TCPClientSocket.recv(buffer_size)
         print(respuesta.decode())
